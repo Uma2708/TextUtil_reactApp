75ca465d5363788d10a704f7f727dc70cf5cfacf
Uma2708 <bhardwajuma001@gmail.com>
2023-03-02 20:38:59 +0530

    Updates

---
+++ precache-manifest.3585242d85d82890d5dc391cf15bff09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecc9360555e2d6e197ed",
+    "url": "//github.com/Uma2708.github.io/TextUtil_reactApp/static/css/main.c7edfe6f.chunk.css"
+  },
+  {
+    "revision": "ecc9360555e2d6e197ed",
+    "url": "//github.com/Uma2708.github.io/TextUtil_reactApp/static/js/main.ecc93605.chunk.js"
+  },
+  {
+    "revision": "bac56d812f0d60920f69",
+    "url": "//github.com/Uma2708.github.io/TextUtil_reactApp/static/js/1.bac56d81.chunk.js"
+  },
+  {
+    "revision": "dbeeb9e4f2ad44aef14f",
+    "url": "//github.com/Uma2708.github.io/TextUtil_reactApp/static/js/runtime~main.dbeeb9e4.js"
+  },
+  {
+    "revision": "e8a08797d4501a777f3c89d96873d2ae",
+    "url": "//github.com/Uma2708.github.io/TextUtil_reactApp/index.html"
+  }
+];
